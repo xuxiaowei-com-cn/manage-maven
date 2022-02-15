d0f38d136aecc1f57697fc6c3a6d0ba028fb33c7
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-02-15 18:06:43 +0800

    日志

--- manage_maven.py
+++ manage_maven.py
@@ -1,5 +1,8 @@
 # -*- coding:utf-8 -*-
 import ctypes
+import logging.handlers
+import os
+import time
 import tkinter.messagebox
 from tkinter.filedialog import askdirectory
 
@@ -9,7 +12,55 @@ class ManageMaven:
     管理 Maven
     """
 
+    def __log__(self):
+        """日志配置"""
+
+        self.FMT = "%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"
+
+        # 日志文件夹
+        self.LOGGING_DIRECTORY = os.path.join(os.getenv("APPDATA"), 'manage_maven', 'log',
+                                              time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()))
+
+        # 判断文件是否不存在
+        if not os.path.exists(self.LOGGING_DIRECTORY):
+            # 创建文件夹
+            os.makedirs(self.LOGGING_DIRECTORY)
+
+        # 获取日志记录器
+        logger = logging.getLogger()
+        # 日志等级
+        logger.setLevel(logging.DEBUG)
+
+        # 全部日志
+        all_handler = logging.handlers.TimedRotatingFileHandler(os.path.join(self.LOGGING_DIRECTORY, 'all.log'))
+        all_handler.setFormatter(logging.Formatter(self.FMT))
+        logger.addHandler(all_handler)
+
+        # 正常日志
+        into_handler = logging.handlers.TimedRotatingFileHandler(os.path.join(self.LOGGING_DIRECTORY, 'info.log'))
+        into_handler.setLevel(logging.INFO)
+        into_handler.setFormatter(logging.Formatter(self.FMT))
+        logger.addHandler(into_handler)
+
+        # 异常日志
+        error_handler = logging.FileHandler(os.path.join(self.LOGGING_DIRECTORY, 'error.log'))
+        error_handler.setLevel(logging.ERROR)
+        error_handler.setFormatter(logging.Formatter(self.FMT))
+        logger.addHandler(error_handler)
+
+        # 创建一个日志处理程序，用于将日志输出到控制台
+        sh = logging.StreamHandler()
+        # 设置输出到控制台的日志等级
+        sh.setLevel(logging.DEBUG)
+        # 添加处理程序
+        logger.addHandler(sh)
+
     def __init__(self):
+        """
+        初始化
+        """
+        self.__log__()
+
         self.root = tkinter.Tk()
         self.root.title("管理 Maven")
 
@@ -49,12 +100,16 @@ class ManageMaven:
         self.upload_button = tkinter.Button(self.root, text="上传文件", command=self.upload_command)
         self.upload_button.pack()
 
+        logging.info('程序启动...')
+        logging.debug(f'日志目录：{self.LOGGING_DIRECTORY}')
+
     def askdirectory_command(self):
         """
         打开文件夹
         :return:
         """
         ask_directory = askdirectory()
+        logging.info(f'选择上传文件夹：{ask_directory}')
 
         if ask_directory != '':
             # 清空
