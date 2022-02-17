6e7f67dd9f54a4921d9352eef0e54642935a0d4b
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-02-17 19:17:10 +0800

    User-Agent

--- manage_maven.py
+++ manage_maven.py
@@ -387,6 +387,8 @@ class ManageMaven:
         authorization = basic(self.username_entry.get(), self.password_entry.get())
 
         headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/98.0.4758.102 Safari/537.36',
             'Authorization': authorization
         }
 
